a3155a5b9f520163a661b9fad03ade8d5b0cf231
prajyot-porje <prajyotyt18@gmail.com>
2024-10-12 18:52:28 +0530

    uploading basic layout of Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/home";
+import Left from "./components/left";
+// import Searchpage from "./components/Searchpage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <body class="bg-black-50">
+        <div class="flex text-white h-[100vh]">
+          <Left/>         
+          <Home/>
+        </div>
+      </body>
+    </>
   );
 }
 
